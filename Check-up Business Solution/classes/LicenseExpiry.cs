e425f21661c0cf86f8ed2e2278a01a6eaaa88f27
jhunexjun <jdream_catcher@yahoo.com>
2015-04-22 15:17:39 +0800

    This is about licensing issue. #25

--- Check-up Business Solution/classes/LicenseExpiry.cs
+++ Check-up Business Solution/classes/LicenseExpiry.cs
@@ -3,28 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace Check_up.classes
 {
     class LicenseExpiry
     {
-        private DateTime expiryDate = new DateTime();
+        string result_expiryDate;
+        DateTime expiryDate = new DateTime();
 
         public LicenseExpiry()
         {
-            string sql = "SELECT licenseExpiry FROM terminal";
-            MySqlCommand cmd = new MySqlCommand(sql, vars.MySqlConnection);
-            string result_expiryDate = (string)cmd.ExecuteScalar();
-            result_expiryDate = CryptorEngine.Decrypt(result_expiryDate);
-            expiryDate = Convert.ToDateTime(result_expiryDate);
+            MySqlCommand cmd = new MySqlCommand("SELECT licenseExpiry FROM `terminal`", vars.MySqlConnection);
+            result_expiryDate = (string)cmd.ExecuteScalar();
+        }
+
+        private bool decrypt(string result_expiry)
+        {
+            try
+            {
+                result_expiry = CryptorEngine.Decrypt(result_expiry);
+                expiryDate = Convert.ToDateTime(result_expiry);
+                return true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(Form.ActiveForm, e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
         }
 
         public bool expiredLicense()
         {
-            DateTime currentDate = DateTime.Today.Date;
-            int i = DateTime.Compare(expiryDate, currentDate);
-            if (i < 0)
-                return true;
+            if (result_expiryDate.Trim() == "")
+                return false;
+            else if (this.decrypt(result_expiryDate))
+            {
+                DateTime currentDate = DateTime.Today.Date;
+                int i = DateTime.Compare(expiryDate, currentDate);
+                if (i < 0)
+                    return true;
+                else
+                    return false;
+            }
             else
                 return false;
         }
